42818dfadb0641c369f0048bd4163d727edec97c
deboracosilveira <deboracosilveira@gmail.com>
2020-05-27 14:38:24 -0300

    Jest Assíncrono

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(true).then(message => expect(message).toBe('Oi!'));
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(false).catch(message => expect(message).toBe('Infelizmente não podemos atender...'));
   });
 });
