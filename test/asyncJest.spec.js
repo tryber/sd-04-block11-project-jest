2b46b295b066074932e0282e425aa7861cf7e6c5
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-05-27 15:11:59 -0300

    second requirement

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,15 +1,5 @@
 const answerPhone = require("../src/asyncJest");
 
-/*
-A função answerPhone recebe um parâmetro boleano.
-Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
-
-Complete o código abaixo para testar as situações em que
-a função recebe como parâmetro true e false, respectivamente.
-
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
-*/
-
 describe("o retorno do telefonema", () => {
   test("atende", (done) => {
     expect(answerPhone(true)).resolves.toBe('Oi!')
@@ -20,3 +10,4 @@ describe("o retorno do telefonema", () => {
     done();
    });
 });
+
