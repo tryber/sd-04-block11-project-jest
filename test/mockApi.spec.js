cc732d8feb91d5ccc4c4521819bc22c9df6317e2
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-05-27 15:36:25 -0300

    third requirement

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,12 +23,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  const user {
+  const user = {
     gender: 'male',
     name: { first: 'Antônio', last: 'Britto' },
     location: {
       country: 'Brazil',
-    }
+    },
       email: 'tunico@bol.com.br',
     login: {
       username: 'tunicao123',
@@ -49,4 +49,4 @@ describe('verifica o usuário', () => {
       expect(user.login.password).toEqual('1234567890');
     });
   });
-}
+})
