684b45444a40910d56a551331ca8fe26653ba6be
juniomelos <you@example.com>
2020-05-27 15:38:41 -0300

    Requirement 2 - mockFunctions - refactorinf function factorial

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -66,11 +66,7 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
   mockFunctions.divide.mockImplementation((a, b) => a / b);
   mockFunctions.power.mockImplementation((a, b) => Math.pow(a, b));
-  mockFunctions.factorial.mockImplementation(a => {
-    let fact = a;
-    for (let i = 1; i < a; i += 1) {
-      fact *= i;
-    }
-    return fact;
-  });
+  mockFunctions.factorial.mockImplementation(a =>
+    a === 0 ? 1 : a * mockFunctions.factorial(a - 1),
+  );
 });
