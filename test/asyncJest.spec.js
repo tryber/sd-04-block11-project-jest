d43c332d8632e109e571af08b4d447d7ed82bd8d
cacobribeiro <cacobribeiro@gmail.com>
2020-05-27 16:51:50 -0300

    Requisito 1 finalizado!

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    expect(await answerPhone(true)).toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    answerPhone(false).catch(e => expect(e).toBe('Infelizmente não podemos atender...'));
   });
 });
