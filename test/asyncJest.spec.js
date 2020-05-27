8f95cf1f2f2ee47165d1df7c7f0af435ff74c040
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-05-27 15:57:09 -0300

    asyncJest half

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", done => {
+    // assert.fail();
+    answerPhone('oi').then((data) => {
+      expect(data).toBe('Oi!');
+      done();
+    })
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    // assert.fail();
+    answerPhone().then((data) => {
+      expect(data).not.toBe('Oi!');
+    })
   });
 });
