21ad74f6b060c443e639f5178f298362b69e4731
Leandro Marten <leandromarten1@gmail.com>
2020-05-27 14:25:05 -0300

    Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,14 +11,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende",  async () => {
+    // assert.fail();
+    const ligacao = await answerPhone(true);
+    expect(ligacao).toEqual('Oi!');
+ 
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    // assert.fail();
+    try {
+      await answerPhone(false);
+    } catch (error){
+      expect(error).toBe('Infelizmente não podemos atender...');
+    }
   });
 });
-
-// iniciando projeto
