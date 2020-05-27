c071d60a493c7f1782e131b829f4a007dc2f8cb0
Ana Higo <anahigo.arq@gmail.com>
2020-05-27 18:27:21 -0300

    3?

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -40,7 +40,7 @@ describe('verifica o usuário', () => {
     },
   };
   api.fetchURL = jest.fn().mockResolvedValue(tunico);
-  test('verifica se o usuário é o tunico', async () => {
+  test("verifica se o usuário é o tunico", async () => {
     return api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -14,9 +14,9 @@ O foco aqui é a utilização de mock functions.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+jest.mock('../src/mockFunctions');
+
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
-    
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
