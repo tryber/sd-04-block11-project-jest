8f5e7e82ce2383ff24eb5bf4cc72ff8652974721
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-27 14:54:01 -0300

    done 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -2,9 +2,11 @@ const answerPhone = require('../src/asyncJest');
 
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
+    expect.assertions(1);
     await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', async () => {
+    expect.assertions(1);
     await expect(answerPhone(false)).rejects.toBe(
       'Infelizmente não podemos atender...',
     );
