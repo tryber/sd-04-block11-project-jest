cdece0ebdc586b1548a6e9f78e0b4eb1f0495f11
igorgbr <igorgphotoarte@gmail.com>
2020-05-27 18:09:43 -0300

    requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,7 +21,8 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
+beforeEach(() => adventure.randomAttack());
+afterEach(() => console.log(adventure.specialists[0].nome))
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
