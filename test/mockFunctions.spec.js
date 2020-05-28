b75641583b70845745d791732db0cee3840f0397
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-05-28 18:56:14 -0300

    Requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -13,11 +13,29 @@ O foco aqui é a utilização de mock functions.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+jest.mock('../src/mockFunctions');
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
-  
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => Math.pow(a, b))
+  mockFunctions.factorial.mockImplementation(a => {
+      let fat = 1
+      for (let i=1; i<=a; i++) {
+        fat *= i
+      }
+      return fat
+    });
+
   test('testa função add', () => {
+    mockFunctions.add(1,2);
+
+    expect(mockFunctions.add).toHaveBeenCalled();
+    expect(mockFunctions.add).toHaveBeenCalledTimes(1);
+    expect(mockFunctions.add).toHaveBeenCalledWith(1,2);
+
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
     expect(mockFunctions.add(-11, 25)).toEqual(14);
@@ -25,6 +43,12 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.add(7, 26)).toEqual(33);
   });
   test('testa função subtract', () => {
+    mockFunctions.subtract(1,2);
+
+    expect(mockFunctions.subtract).toHaveBeenCalled();
+    expect(mockFunctions.subtract).toHaveBeenCalledTimes(1);
+    expect(mockFunctions.subtract).toHaveBeenCalledWith(1,2);
+
     expect(mockFunctions.subtract(899, 35)).toEqual(864);
     expect(mockFunctions.subtract(-17, 333)).toEqual(-350);
     expect(mockFunctions.subtract(45, 97)).toEqual(-52);
@@ -32,6 +56,12 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.subtract(-133, -29)).toEqual(-104);
   });
   test('testa função multiply', () => {
+    mockFunctions.multiply(1,2);
+
+    expect(mockFunctions.multiply).toHaveBeenCalled();
+    expect(mockFunctions.multiply).toHaveBeenCalledTimes(1);
+    expect(mockFunctions.multiply).toHaveBeenCalledWith(1,2);
+    
     expect(mockFunctions.multiply(1, 2)).toEqual(2);
     expect(mockFunctions.multiply(0, 5)).toEqual(0);
     expect(mockFunctions.multiply(-4, 9)).toEqual(-36);
@@ -39,6 +69,12 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.multiply(19, 23)).toEqual(437);
   });
   test('testa função divide', () => {
+    mockFunctions.divide(1,2);
+
+    expect(mockFunctions.divide).toHaveBeenCalled();
+    expect(mockFunctions.divide).toHaveBeenCalledTimes(1);
+    expect(mockFunctions.divide).toHaveBeenCalledWith(1,2);
+
     expect(mockFunctions.divide(169, 13)).toEqual(13);
     expect(mockFunctions.divide(-1900, 5)).toEqual(-380);
     expect(mockFunctions.divide(42, 7)).toEqual(6);
@@ -46,6 +82,12 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.divide(1331, 11)).toEqual(121);
   });
   test('testa função power', () => {
+    mockFunctions.power(1,2);
+
+    expect(mockFunctions.power).toHaveBeenCalled();
+    expect(mockFunctions.power).toHaveBeenCalledTimes(1);
+    expect(mockFunctions.power).toHaveBeenCalledWith(1,2)
+    
     expect(mockFunctions.power(10, 2)).toEqual(100);
     expect(mockFunctions.power(2, 10)).toEqual(1024);
     expect(mockFunctions.power(5, 5)).toEqual(3125);
@@ -53,6 +95,12 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.power(0, 0)).toEqual(1);
   });
   test('testa função factorial', () => {
+    mockFunctions.factorial(3);
+
+    expect(mockFunctions.factorial).toHaveBeenCalled();
+    expect(mockFunctions.factorial).toHaveBeenCalledTimes(1);
+    expect(mockFunctions.factorial).toHaveBeenCalledWith(3)
+
     expect(mockFunctions.factorial(5)).toEqual(120);
     expect(mockFunctions.factorial(10)).toEqual(3628800);
     expect(mockFunctions.factorial(3)).toEqual(6);
