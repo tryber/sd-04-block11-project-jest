d703646b37efe8272f7c2aae461605b64dba590e
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-05-30 16:31:33 -0300

    requisitos

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -2,4 +2,4 @@
 const answerPhone = answer => new Promise(function (resolve, reject) {
   setTimeout(answer ? resolve('Oi!') : reject('Infelizmente não podemos atender...'), 3000);
 });
-module.exports = answerPhone; "superar"
+module.exports = answerPhone;
