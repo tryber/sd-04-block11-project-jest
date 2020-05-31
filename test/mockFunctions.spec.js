0b566f6a079846e2e0288a9c94a64c267800980b
Caio Magalhães <caio.lmf@gmail.com>
2020-05-31 16:09:39 -0300

    Add mockfunctions tests

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const mockFunctions = require('../src/mockFunctions');
 
 /*
@@ -15,8 +16,20 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
-  
+  mockFunctions.add = jest.fn((a, b) => a + b);
+  mockFunctions.subtract = jest.fn((a, b) => a - b);
+  mockFunctions.multiply = jest.fn((a, b) => a * b);
+  mockFunctions.divide = jest.fn((a, b) => a / b);
+  mockFunctions.power = jest.fn((a, b) => a ** b);
+  mockFunctions.factorial = jest.fn((a) => {
+    let result = 1;
+    while (a > 0) {
+      result *= a;
+      a -= 1;
+    }
+    return result;
+  });
+
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
